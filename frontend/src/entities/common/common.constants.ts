cfe87a4bc35897a93277888321498df5f3a3a06f
prefie <prefie@bk.ru>
2023-05-07 15:42:50 +0500

    Ограничения для раундов-вопросов на бэке, константа на фронте для демо-админа

--- frontend/src/entities/common/common.constants.ts
+++ frontend/src/entities/common/common.constants.ts
@@ -1,4 +1,5 @@
 export const userRoles: string[] = ['user'];
 export const superAdminRoles: string[] = ['superadmin'];
+export const demoAdminRoles: string[] = ['demoadmin'];
 export const realAdminRoles: string[] = superAdminRoles.concat(['admin']);
-export const allAdminRoles: string[] = realAdminRoles.concat(['demoadmin']);
+export const allAdminRoles: string[] = realAdminRoles.concat(demoAdminRoles);
