d2c4dd1f2d9a150ea2ed5719ae4e74f5e9b2defc
Соколов Алексей <av.sok@skbkontur.ru>
2023-12-04 16:02:02 +0500

    fix jwt token exception

--- app/utils/api-error.ts
+++ app/utils/api-error.ts
@@ -1,4 +1,21 @@
+export class APIError extends Error {
+    public constructor(
+        public message: string,
+        public status?: number,
+    ) {
+        super(message);
+        this.status = status || 500;
+
+        Error.captureStackTrace(this, APIError);
+    }
+}
+
 export const errorHandler = (err, req, res, next) => {
     console.error(err?.stack);
-    return res.status(500).json({ message: err.message });
+
+    if (err instanceof APIError) {
+        return res.status(err.status || 500).json({message: err.message});
+    } else {
+        return res.status(500).json({message: err.message});
+    }
 };
