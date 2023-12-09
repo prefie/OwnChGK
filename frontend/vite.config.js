d0e8b6e8bffe7bd3cc67853e810c39246c7a893b
prefie <prefie@bk.ru>
2023-12-09 13:15:25 +0500

    fix

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,6 +1,6 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
-import svgLoader from 'vite-svg-loader'
+import svgLoader from 'vite-svg-loader';
 
 export default ({ mode }) => {
     return defineConfig({
