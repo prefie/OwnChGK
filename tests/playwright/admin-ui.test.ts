12a08f3a9dbea1b1357a33c88e845434d4a22785
udarenienao <72667318+udarenienao@users.noreply.github.com>
2024-01-15 23:08:35 +0500

    fix

--- tests/playwright/admin-ui.test.ts
+++ tests/playwright/admin-ui.test.ts
@@ -1,98 +1,93 @@
-import { ADMIN_URL, LOGIN_USER_SECRET, login } from "../test-helper";
+import { ADMIN_URL, LOGIN_USER_SECRET } from "../test-consts";
 
-import { Page, firefox } from 'playwright';
 import { test, expect } from '@playwright/test'
+import { LoginPage } from "./pages/login-page";
+import { RestorePasswordPage } from "./pages/restore-password-page";
+import { StartScreenPage } from "./pages/start-screen-page";
+import { TeamCreationPage } from "./pages/team-creation-page";
+import { ProfilePage } from "./pages/profile-page";
 
-let page: Page;
-
-test.beforeEach(async () => {
-    const browser = await firefox.launch();
-    const context = await browser.newContext();
-    page = await context.newPage();
+test.beforeEach(async ({page}) => {
     await page.goto(ADMIN_URL);
     await page.waitForLoadState('networkidle');
 });
 
-test('Should_open_admin_page', async () => {
+test('Should_open_admin_page', async ({page}) => {
     const currentUrl = page.url();
     expect.soft(currentUrl).toContain(ADMIN_URL);
 });
 
-test('Should_successful_login', async () => {
-    await login(page);
-    await page.waitForURL('**/start-screen');
+test('Should_successful_login', async ({page}) => {
+    const loginPage = new LoginPage(page);
+    await loginPage.login();
+ 
+    const startScreenPage = new StartScreenPage(page);
+    await startScreenPage.accept();
 });
 
-test('Should_go_to_change_password', async () => {
-    const restoreLink = page.locator('#restore');
-    await restoreLink?.click();
-    await page.waitForURL('**/restore-password');
+test('Should_go_to_change_password', async ({page}) => {
+    const loginPage = new LoginPage(page);
+    await loginPage.restoreLink.click();
 
-    const button = page.locator('#restoreSend');
-    const input = page.locator('#email');
-    const rememberPasswordLink = page.locator('#remember');
-
-    expect.soft(await input?.getAttribute("placeholder")).toBe("Почта");
-    await expect.soft(button).toHaveText('Отправить');
-    expect.soft(await rememberPasswordLink?.getAttribute("href")).toContain("/admin");
-    await expect.soft(rememberPasswordLink).toHaveText("Вспомнил пароль");
+    const restorePasswordPage = new RestorePasswordPage(page);
+    await restorePasswordPage.accept();
+  
+    expect.soft(await restorePasswordPage.emailInput.getAttribute("placeholder")).toBe("Почта");
+    await expect.soft(restorePasswordPage.restoreSendButton).toHaveText('Отправить');
+    expect.soft(await restorePasswordPage.rememberPasswordLink.getAttribute("href")).toContain("/admin");
+    await expect.soft(restorePasswordPage.rememberPasswordLink).toHaveText("Вспомнил пароль");
 });
 
-test('Should_go_to_team_creation_by_admin', async () => {
-    await login(page);
-    await page.waitForURL('**/start-screen');
+test('Should_go_to_team_creation_by_admin', async ({page}) => {
+    const loginPage = new LoginPage(page);
+    await loginPage.login();
 
-    const teamsTab = page.locator('#teams');
-    await teamsTab?.click();
-    const button = page.locator('#addTeamButton');
-    await button?.click();
+    const startScreenPage = new StartScreenPage(page);
+    await startScreenPage.accept();
 
-    const teamNameInput = page.locator('#teamName');
-    const captainInput = page.locator('#captain');
-    const saveTeamButton = page.locator('#saveTeam');
+    await startScreenPage.teamsTab.click();
+    await startScreenPage.addTeamButton.click();
 
-    const currentUrl = page.url();
-    expect.soft(currentUrl).toContain('/team-creation');
-    expect.soft(await teamNameInput?.getAttribute("placeholder")).toBe("Название команды");
-    await expect.soft(saveTeamButton).toHaveText("Создать");
+    const teamCreationPage = new TeamCreationPage(page);
+    await teamCreationPage.accept();
+
+    expect.soft(await teamCreationPage.teamNameInput.getAttribute("placeholder")).toBe("Название команды");
+    await expect.soft(teamCreationPage.saveTeamButton).toHaveText("Создать");
 });
 
-test('Should_go_to_admin_profile', async () => {
-    await login(page);
-    await page.waitForURL('**/start-screen');
+test('Should_go_to_admin_profile', async ({page}) => {
+    const loginPage = new LoginPage(page);
+    await loginPage.login();
 
-    const profile = page.locator('#profile');
-    await profile?.click();
-    const email = page.locator('#email');
+    const startScreenPage = new StartScreenPage(page);
+    await startScreenPage.accept();
 
-    const oldPassword = page.locator('#old-password');
-    const newPassword = page.locator('#new-password');
-    const newPasswordRepeat = page.locator('#repeat-new-password');
-    const saveButton = page.locator('#saveProfile');
+    await startScreenPage.profile.click();
 
-    await expect.soft(email).toHaveText(LOGIN_USER_SECRET);
-    expect.soft(await oldPassword?.getAttribute("value")).toBe("");
-    expect.soft(await newPassword?.getAttribute("value")).toBe("");
-    expect.soft(await newPasswordRepeat?.getAttribute("value")).toBe("");
-    await expect.soft(saveButton).toHaveText("Сохранить");
+    const profilePage = new ProfilePage(page);
+    profilePage.accept();
+
+    await expect.soft(profilePage.email).toHaveText(LOGIN_USER_SECRET);
+    expect.soft(await profilePage.oldPassword.getAttribute("value")).toBe("");
+    expect.soft(await profilePage.newPassword.getAttribute("value")).toBe("");
+    expect.soft(await profilePage.newPasswordRepeat.getAttribute("value")).toBe("");
+    await expect.soft(profilePage.saveButton).toHaveText("Сохранить");
 });
 
-test('Should_admin_logout', async () => {
-    await login(page);
-    await page.waitForURL('**/start-screen');
+test('Should_admin_logout', async ({page}) => {
+    const loginPage = new LoginPage(page);
+    await loginPage.login();
+
+    const startScreenPage = new StartScreenPage(page);
+    await startScreenPage.accept();
 
     const cookie = await page.context().cookies();
     expect(cookie.find(c => c.name === "authorization")).toBeDefined();
 
-    const logout = page.locator('img[alt="LogOut"]');
-    await logout?.click();
+    await startScreenPage.logoutButton.click();
 
     const currentUrl = page.url();
     expect.soft(currentUrl).toContain(ADMIN_URL);
     const cookieAfterLogout = await page.context().cookies();
     expect.soft(cookieAfterLogout.find(c => c.name === "authorization")).toBeUndefined();
-});
-
-test.afterEach(async () => {
-    await page.close();
 });
